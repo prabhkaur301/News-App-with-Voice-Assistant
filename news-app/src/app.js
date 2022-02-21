939b53a026cf98ed1877f7cf5ec1475aee96a8e4
prabhkaur301 <pk746467@gmail.com>
2022-02-21 14:33:00 +0530

    Dependendies added

---
+++ news-app/src/app.js
@@ -0,0 +1,22 @@
+import React,{useEffect} from "react"
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const apiKey="e92bd3d581cc061e987e36cd076e64902e956eca572e1d8b807a3e2338fdd0dc/stage"
+const App=()=>{
+
+    useEffect(()=>{
+        alanBtn({
+            key:apiKey,
+            onCommand: (commandData) => {
+                if (commandData.command === 'go:back') {
+                    // Call the client code that will react to the received command
+                }
+            }
+        })
+    },[])
+
+    return(
+        <h1>Hi</h1>
+    )
+}
+export default App;
